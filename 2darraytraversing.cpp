cde4dac2deb08fcc238b3ba18d7d47ef49b35777
Rajat7667 <63415041+Rajat7667@users.noreply.github.com>
2020-07-01 21:53:10 +0530

    my first C++
    hackerrank

---
+++ 2darraytraversing.cpp
@@ -0,0 +1,62 @@
+
+324#include<stdio.h>
+int main()
+{
+int r,c,a[3][4],b[3][4],s[3][4];
+for(r=0;r<3;r++)
+
+	for(c=0;c<4;c++)
+
+		scanf("%d",&a[r][c]);
+	
+	
+	for(r=0;r<3;r++)
+
+
+
+	for(c=0;c<4;c++)
+	
+				scanf("%d",&b[r][c]);
+		
+		
+	
+
+ 
+
+
+for(r=0;r<3;r++)
+
+{
+
+	for(c=0;c<4;c++)
+	{
+	
+				s[r][c]=a[r][c]+b[r][c];
+			}
+		
+		
+}
+
+ 
+for(r=0;r<3;r++)
+{
+
+	for(c=0;c<4;c++)
+	
+	
+
+	printf("%d",s[r][c]);
+	
+printf("\n");	
+}
+	
+	
+	
+	
+ 
+}
+	
+
+	
+
+
